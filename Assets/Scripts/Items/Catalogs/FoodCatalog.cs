395d1609b780977395cf98e30f786bd1e26c2504
BeaverWithBerets <pavel.p1112@gmail.com>
2024-07-07 17:25:54 +0300

    Bug fix

--- Assets/Scripts/Items/Catalogs/FoodCatalog.cs
+++
@@ -1,1 +0,0 @@
-public class FoodCatalog : Catalog<Food> { }
--- Assets/Scripts/Items/Food.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Food", menuName = "Items/Food")]
-public class Food : Item
-{
-    [field: SerializeField]
-    public int calorie { get; private set; }
-}
